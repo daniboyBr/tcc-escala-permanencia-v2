ff7e00c041c9330cc5a598eee41ae95cf6020833
Daniel Envagelista Pereira <daniel.evangelista93@gmail.com>
2022-01-23 18:19:12 -0300

    criado model e migration da organizacao militar

---
+++ database/migrations/2022_01_23_211424_create_organizacao_militars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizacaoMilitarsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('organizacaoMilitar', function (Blueprint $table) {
+			$table->id();
+			$table->string('nome', 60);
+			$table->string('sigla', 10);
+			$table->boolean('flgAtivo')->default(true);
+			$table->string('inseridoPor', 100)->nullable();
+			$table->string('atualizadoPor', 100)->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('organizacaoMilitar');
+	}
+}
