fa8b1122c2285ab71818d7430bd972b30eca1b60
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2022-02-02 23:17:27 -0300

    adicionando perfect scollbar como dependencia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
 import $ from 'jquery';
 import Popper from 'popper.js';
+import PerfectScrollbar from 'perfect-scrollbar';
+
 window.Popper = Popper;
 window.jQuery = $;
 window.$ = $;
+
+window.$.fn.perfectScrollbar = function (options) {
+
+    return this.each((k, elm) => new PerfectScrollbar(elm, options || {}));
+};
+
 require('./bootstrap');
